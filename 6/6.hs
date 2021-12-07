b87625c736ec26bdd5fa0596d2d5a70d7233c166
sylbru <sylvain.brunerie@gmail.com>
2021-12-07 01:40:00 +0100

    Attempt to switch to 4 bits per fish, and rename "optimized" functions

--- 6/6.hs
+++ 6/6.hs
@@ -1,8 +1,7 @@
 import Data.Char (isSpace)
 import Data.List (unfoldr)
 
-type LanternfishAges = [Int]
-type OptimizedLanternfishAges = Int
+type LanternfishAges = Int
 
 testInput :: String
 testInput = "3,4,3,1,2\n"
@@ -28,79 +27,63 @@ split delimiter string =
                 (item, rest) ->
                     Just (item, drop 1 rest)
 
+
 parseInput :: String -> LanternfishAges
 parseInput =
-    map read . split ',' . trim
+    foldr (\acc i -> acc + 16 * i) 0 . reverse . map read . split ',' . trim
 
-parseInputOptimized :: String -> OptimizedLanternfishAges
-parseInputOptimized =
-    foldr (\acc i -> acc + 8 * i) 0 . reverse . parseInput
-  
+
 nextDay :: LanternfishAges -> LanternfishAges
-nextDay ages =
-    let
-        births = length . filter (== 0) $ ages
-    in
-    (replicate births 8) ++ (map nextDaySingle ages)
-
-nextDayOptimized :: OptimizedLanternfishAges -> OptimizedLanternfishAges
-nextDayOptimized optimizedAges =
-    -- nextDayOptimized 14538 = 9857
-    -- nextDayOptimized 9857 = 41864 (pas forcément : l’ordre peut être différent, donc la représentation aussi)
-    -- countFish (nextDayOptimized 9857) == countFish 41864
-    go 0 optimizedAges
+nextDay ages_ =
+    -- nextDay 14538 = 9857
+    -- nextDay 9857 = 41864 (pas forcément : l’ordre peut être différent, donc la représentation aussi)
+    -- countFish (nextDay 9857) == countFish 41864
+    go 0 ages_
     where
         go col ages =
             if ages == 0 then
                 0
             else
-                if ages `mod` 8 > 0 then
-                    (go (col + 1) (ages `div` 8))
-                        + (8 ^ col) * (ages `mod` 8 - 1) 
+                if ages `mod` 16 > 0 then
+                    (go (col + 1) (ages `div` 16))
+                        + (16 ^ col) * (ages `mod` 16 - 1) 
                 else
-                    (go (col + 2) (ages `div` 8))
-                    -- + (6 et 8)
+                    (go (col + 2) (ages * 16))
+                    + 104 -- (6 et 8)
 
-nextDaySingle :: Int -> Int
-nextDaySingle age =
-    if age < 1 then
-        6
-    else
-        age - 1
-
-atDay :: Int -> OptimizedLanternfishAges -> OptimizedLanternfishAges
+atDay :: Int -> LanternfishAges -> LanternfishAges
 atDay d ages =
     if d <= 0 then
         ages
     else
-        atDay (d - 1) (nextDayOptimized ages)
+        atDay (d - 1) (nextDay ages)
 
-optimizedAgesToList :: OptimizedLanternfishAges -> [Int]
-optimizedAgesToList ages =
+agesToList :: LanternfishAges -> [Int]
+agesToList ages =
     reverse (unfoldr f ages)
     where
         f :: Int -> Maybe (Int, Int)
         f v =
-            case (v `div` 8, v `mod` 8) of
+            case (v `div` 16, v `mod` 16) of
                 (0, 0) ->
                     Nothing
 
                 (_, remainder) ->
-                    Just (remainder, (v - remainder) `div` 8)
+                    Just (remainder, (v - remainder) `div` 16)
 
-countFish :: OptimizedLanternfishAges -> Int
-countFish optimizedAges =
-    go 0 optimizedAges
+countFish :: LanternfishAges -> Int
+countFish ages_ =
+    go 0 ages_
     where
-        go :: Int -> OptimizedLanternfishAges -> Int
+        go :: Int -> LanternfishAges -> Int
         go acc ages =
             if ages <= 0 then
                 acc
             else
-                go (acc + 1) (ages `div` 8)
+                go (acc + 1) (ages `div` 16)
 
 
 main :: IO ()
 main = do
     raw <- getContents
-    print . countFish . atDay 80 . parseInputOptimized $ testInput
+    print . countFish . atDay 80 . parseInput $ testInput
