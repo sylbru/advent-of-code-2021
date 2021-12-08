4530abe3df6a9ae7af0961889490cdaf88995ff4
sylbru <sylvain.brunerie@gmail.com>
2021-12-09 00:12:10 +0100

    Day 8, part one

---
+++ 8/8.hs
@@ -0,0 +1,86 @@
+import Data.List (elem)
+import Data.List.Split (splitOn)
+
+{-
+0: abcefg  (6)
+1: cf      (2) #easy
+2: acdeg   (5)
+3: acdfg   (5)
+4: bcdf    (4) #easy
+5: abdfg   (5)
+6: abdefg  (6)
+7: acf     (3) #easy
+8: abcdefg (7) #easy
+9: abcdfg  (6)
+-}
+
+data Digit = D0 | D1 | D2 | D3 | D4 | D5 | D6 | D7 | D8 | D9
+data Segment = A | B | C | D | E | F | G
+type DisplayedDigit = [Segment]
+type Entry = ([DisplayedDigit], [DisplayedDigit])
+
+
+parseInput :: String -> [Entry]
+parseInput input =
+    map parseEntry (lines input)
+
+parseEntry :: String -> Entry
+parseEntry input =
+    let
+        (rawSignalPatterns, rawOutputValue) = listTo2Tuple (splitOn " | " input)
+    in
+    (parseDigits rawSignalPatterns, parseDigits rawOutputValue)
+
+parseDigits :: String -> [DisplayedDigit]
+parseDigits raw =
+    map parseDigit $ words raw
+
+parseDigit :: String -> DisplayedDigit
+parseDigit word =
+    map parseSegment word
+
+parseSegment :: Char -> Segment
+parseSegment char =
+    case char of
+        'a' -> A
+        'b' -> B
+        'c' -> C
+        'd' -> D
+        'e' -> E
+        'f' -> F
+        'g' -> G
+
+listTo2Tuple :: [a] -> (a, a)
+listTo2Tuple (a1:a2:_) = (a1, a2)
+
+countEasyDigits :: [Entry] -> Int
+countEasyDigits = sum . map countEasyDigitsForEntry
+
+countEasyDigitsForEntry :: Entry -> Int
+countEasyDigitsForEntry (_, outputValue) =
+    length $ filter isEasyDigit outputValue
+
+isEasyDigit :: DisplayedDigit -> Bool
+isEasyDigit displayedDigit =
+    elem
+        (length displayedDigit)
+        (map (length . segmentsForDigit) [D1, D4, D7, D8])
+
+segmentsForDigit :: Digit -> DisplayedDigit
+segmentsForDigit digit =
+    case digit of
+        D0 -> [A, B, C, E, F, G]
+        D1 -> [C, F]
+        D2 -> [A, C, D, E, G]
+        D3 -> [A, C, D, F, G]
+        D4 -> [B, C, D, F]
+        D5 -> [A, B, D, F, G]
+        D6 -> [A, B, D, E, F, G]
+        D7 -> [A, C, F]
+        D8 -> [A, B, C, D, E, F, G]
+        D9 -> [A, B, C, D, F, G]
+
+main :: IO ()
+main = do
+    raw <- getContents
+    print . countEasyDigits . parseInput $ raw
