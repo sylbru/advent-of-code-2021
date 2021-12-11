29bfc5474f61940c9dc65591a96194ea7a8b6c85
sylbru <sylvain.brunerie@gmail.com>
2021-12-11 01:44:44 +0100

    Day 10, part two + much cleaner code

--- 10/10.hs
+++ 10/10.hs
@@ -1,7 +1,20 @@
+import Data.List (sort)
+import Data.Maybe (fromJust, mapMaybe)
+
 data Diagnostic
     = Corrupted Char -- Syntax error on this char
     | Incomplete [Char] -- Incomplete line with this stack of open chunks
     | Correct
+    deriving (Show)
+
+matchingChars :: [(Char, Char)]
+matchingChars =
+    [ ('(', ')')
+    , ('[', ']')
+    , ('{', '}')
+    , ('<', '>')
+    ]
+
 
 syntaxErrorScore :: String -> Int
 syntaxErrorScore input =
@@ -19,6 +32,42 @@ toErrorScore diagnostics =
                 '>' -> 25137
                 _ -> 0
         value _ = 0
+
+
+completionScore :: String -> Int
+completionScore input =
+    toCompletionScore . map diagnoseLine . lines $ input
+
+toCompletionScore :: [Diagnostic] -> Int
+toCompletionScore diagnostics =
+    let scores = filter (/= 0) $ map value diagnostics
+    in
+    sort scores !! (length scores `div` 2)
+    where
+        value (Incomplete []) = 0
+        value (Incomplete stack) = completionScoreLine . autocomplete $ stack
+        value _ = 0
+
+completionScoreLine :: [Char] -> Int
+completionScoreLine =
+    foldl fn 0
+    where
+        fn acc char =
+            acc * 5
+                + (case char of
+                    ')' -> 1
+                    ']' -> 2
+                    '}' -> 3
+                    '>' -> 4
+                    _ -> 0
+                )
+
+autocomplete :: [Char] -> [Char]
+autocomplete openChunks =
+    mapMaybe matchingChar openChunks
+    where
+        matchingChar openingChar =
+            lookup openingChar matchingChars
 
 diagnoseLine :: String -> Diagnostic
 diagnoseLine [] = Correct
@@ -41,21 +90,17 @@ diagnoseLine (stackInit:line) =
                 go stack lineRest
 
 isOpeningChar :: Char -> Bool
-isOpeningChar c = elem c ['(', '[', '{', '<']
+isOpeningChar c = elem c (map fst matchingChars)
 
 isClosingChar :: Char -> Bool
-isClosingChar c = elem c [')', ']', '}', '>']
+isClosingChar c = elem c (map snd matchingChars)
 
 matchesOpeningChar :: Char -> Char -> Bool
-matchesOpeningChar c s =
-    case (s, c) of
-        ('(', ')') -> True
-        ('[', ']') -> True
-        ('{', '}') -> True
-        ('<', '>') -> True
-        _ -> False
+matchesOpeningChar c s = elem (s, c) matchingChars
+
 
 main :: IO ()
 main = do
     raw <- getContents
     print . syntaxErrorScore $ raw
+    print . completionScore $ raw
