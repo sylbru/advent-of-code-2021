45d513de4aac1e0119ae12e8c53ffd29bff789a7
sylbru <sylvain.brunerie@gmail.com>
2021-12-12 00:54:11 +0100

    Let’s use a List actually

--- 11/11.hs
+++ 11/11.hs
@@ -1,17 +1,17 @@
 import Data.Array
 
 type EnergyLevel = Int
-type Cave = Array Int (Array Int EnergyLevel)
+type Cave = [[EnergyLevel]]
 
 -- example as a single line: 5483143223\n2745854711\n5264556173\n6141336146\n6357385478\n4167524645\n2176841721\n6882881134\n4846848554\n5283751526\n
 
 parseInput :: String -> Cave
 parseInput =
-    listArray (0,9) . map parseLine . lines
+    map parseLine . lines
     where
-        parseLine :: String -> Array Int EnergyLevel
+        parseLine :: String -> [EnergyLevel]
         parseLine =
-            listArray (0,9) . map (read . stringFromChar)
+            map (read . stringFromChar)
 
         stringFromChar :: Char -> String
         stringFromChar = (:[])
