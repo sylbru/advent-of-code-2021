54f22630de03a27c4289562faaf016b938d0d832
sylbru <sylvain.brunerie@gmail.com>
2021-12-21 00:43:32 +0100

    Image as map

--- 20/advent.hs
+++ 20/advent.hs
@@ -1,5 +1,8 @@
+import Data.Map.Strict (Map, fromList, foldrWithKey, lookup)
+-- import Data.List (map)
+
 data Pixel = Light | Dark deriving (Eq)
-type Image = [[Pixel]]
+type Image = Map (Int, Int) Pixel
 type Algorithm = [Pixel]
 
 parseInput :: String -> (Algorithm, Image)
@@ -7,6 +10,29 @@ parseInput input =
     case lines input of
         algorithm:"":image ->
             (parseAlgorithm algorithm, parseImage image)
+
+parseImage :: [String] -> Image
+parseImage input =
+    fromList . concat $
+    indexedMap
+        (\(y, row) ->
+            indexedMap
+                (\(x, cell) ->
+                    ((x,y), parsePixel cell)
+                )
+                row
+        )
+        input
+
+indexedMap :: ((Int, a) -> b) -> [a] -> [b]
+indexedMap f l =
+    go 0 f l
+    where
+        go :: Int -> ((Int, a) -> b) -> [a] -> [b]
+        go _ _ [] = []
+        go i fun (x:xs) =
+            fun (i,x) : go (i + 1) fun xs
+
 
 parsePixel :: Char -> Pixel
 parsePixel char =
@@ -19,18 +45,37 @@ parseAlgorithm :: String -> Algorithm
 parseAlgorithm input =
     map parsePixel input
 
-parseImage :: [String] -> Image
-parseImage input =
-    map (map parsePixel) input
 
 printImage :: Image -> String
 printImage image =
-    unlines . map (map printPixel) $ image
-    where
-        printPixel pixel =
-            case pixel of
-                Light -> '█'
-                Dark -> ' '
+    let
+        minX = foldrWithKey (\(x,_) _ acc -> min acc x) 0 image
+        minY = foldrWithKey (\(_,y) _ acc -> min acc y) 0 image
+        maxX = foldrWithKey (\(x,_) _ acc -> max acc x) 0 image
+        maxY = foldrWithKey (\(_,y) _ acc -> max acc y) 0 image
+        (w, h) = (1 + maxX - minX, 1 + maxY - minY)
+        emptyImage = (replicate h (replicate w Dark))
+
+        imageAsList =
+            indexedMap
+                (\(y, row) ->
+                    indexedMap
+                        (\(x, _) ->
+                            case Data.Map.Strict.lookup (x,y) image of
+                                Just pixel -> pixel
+                                Nothing -> Dark
+                        )
+                        row
+                )
+                emptyImage
+    in
+    unlines . map (map printPixel) $ imageAsList
+
+printPixel :: Pixel -> Char
+printPixel pixel =
+    case pixel of
+        Light -> '█'
+        Dark -> ' '
 
 main :: IO ()
 main = do
