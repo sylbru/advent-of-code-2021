129f87f5246fe6d59e1cfc7ed6aa6551d7b37f70
sylbru <sylvain.brunerie@gmail.com>
2021-12-21 22:36:05 +0100

    Day21: massive perf improvement (?)

--- 21/advent.hs
+++ 21/advent.hs
@@ -1,15 +1,25 @@
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+
+import Data.List
 import Data.Maybe (mapMaybe)
+import Control.DeepSeq (force, NFData)
+import GHC.Generics (Generic)
 
 type Score = Int
-type InitialState = (Position, Position)
+type InitialPositions = (Position, Position)
 type Position = Int
 type DiceState = Int
-data Game = Game { positions :: (Position, Position), scores :: (Score, Score) } deriving (Show)
+data Game = Game
+    { positions :: (Position, Position)
+    , scores :: (Score, Score)
+    , occurrences :: Int
+    }
+    deriving (Eq, Show, Generic, NFData)
 
-example :: InitialState
+example :: InitialPositions
 example = (4, 8)
 
-input :: InitialState
+input :: InitialPositions
 input = (10, 1)
 
 
@@ -23,25 +33,18 @@ roll100 (pos, diceAt) =
     in
     (newPos, newDiceAt)
 
-roll3 :: (Position, DiceState) -> (Position, DiceState)
-roll3 (pos, diceAt) =
-    let
-        diceValue = sum [diceAt..diceAt+2] -- or (diceAt+2)*(diceAt+3) / 2 - diceAt*(diceAt-1) / 2
-                                           -- but who cares?
-        newPos = 1 + ((pos + diceValue - 1)  `mod` 10)
-        newDiceAt = 1 + (diceAt + 3 - 1 `mod` 3)
-    in
-    (newPos, newDiceAt)
-
 initialDice :: DiceState
 initialDice = 1
 
-play1 :: InitialState -> (Score, Score, Int)
-play1 initialState =
-    go initialState (0,0) initialDice True 0
+finalScore1 :: Int
+finalScore1 = 1000
+
+play1 :: InitialPositions -> (Score, Score, Int)
+play1 initialPositions =
+    go initialPositions (0,0) initialDice True 0
     where
         go (pos1, pos2) (score1, score2) diceAt firstPlayersTurn rolls =
-            if score1 >= 1000 || score2 >= 1000 then
+            if score1 >= finalScore1 || score2 >= finalScore1 then
                 (score1, score2, rolls)
             else
                 let
@@ -56,9 +59,12 @@ answer1 :: (Score, Score, Int) -> Int
 answer1 (score1, score2, rolls) =
     rolls * (min score1 score2)
 
-play2 :: InitialState -> (Int, Int)
-play2 initialState =
-    go ((0,0), [Game { positions = initialState, scores = (0,0) }]) True
+finalScore2 :: Int
+finalScore2 = 8
+
+play2 :: InitialPositions -> (Int, Int)
+play2 initialPositions =
+    go ((0,0), [Game { positions = initialPositions, scores = (0,0), occurrences = 1 }]) True
     where
         go :: ((Int, Int), [Game]) -> Bool -> (Int, Int)
         go (winningGames, ongoingGames) firstPlayersTurn =
@@ -67,9 +73,9 @@ play2 initialState =
             else
                 let
                     newOngoingGames :: [Game]
-                    newOngoingGames = concat $ map
+                    newOngoingGames = force $ concat $ map
                         (\game ->
-                            map (\rollSum ->
+                            map (\(rollSum, occ) ->
                                 let
                                     (pos1, pos2) = positions game
                                     (score1, score2) = scores game
@@ -80,53 +86,62 @@ play2 initialState =
                                     newScores = if firstPlayersTurn then
                                         (score1 + (fst newPositions), score2) else (score1, score2 + (snd newPositions))
                                 in
-                                Game { positions = newPositions, scores = newScores}
+                                Game { positions = newPositions, scores = newScores, occurrences = occurrences game * occ }
                             )
                             multiRollsSums
                         )
                         ongoingGames
                 in
-                go (closeGames (winningGames, newOngoingGames)) firstPlayersTurn
+                go (force $ closeGames (winningGames, newOngoingGames)) (not firstPlayersTurn)
 
 
 closeGames :: ((Int, Int), [Game]) -> ((Int, Int), [Game])
 closeGames (wins, ongoing) =
     let
         won = mapMaybe (\game ->
-            if fst (scores game) >= 21 then
-                Just True
-            else if snd (scores game) >= 21 then
-                Just False
+            if fst (scores game) >= finalScore2 then
+                Just (True, occurrences game)
+            else if snd (scores game) >= finalScore2 then
+                Just (False, occurrences game)
             else
                 Nothing
             )
             ongoing
-        newWins = (fst wins + (length $ filter id won), snd wins + (length $ filter (not . id) won))
+        newWins =
+            ( fst wins + (sum $ map snd $ filter (id . fst) won)
+            , snd wins + (sum $ map snd $ filter (not . id . fst) won)
+            )
         newOngoing = filter
-            (\game -> fst (scores game) < 21 && snd (scores game) < 21)
+            (\game -> fst (scores game) < finalScore2 && snd (scores game) < finalScore2)
             ongoing
     in
     (newWins, newOngoing)
 
--- gameOver :: Game -> Bool
--- gameOver (Game { positions }) =
---     fst positions >= 21 || snd positions >= 21
-
-multiRollsSums :: [Int]
+multiRollsSums :: [(Int, Int)]
 multiRollsSums =
-    map sum
-        [ [1,1,1], [1,1,2], [1,1,3]
-        , [1,2,1], [1,2,2], [1,2,3]
-        , [1,3,1], [1,3,2], [1,3,3]
-        , [2,1,1], [2,1,2], [2,1,3]
-        , [2,2,1], [2,2,2], [2,2,3]
-        , [2,3,1], [2,3,2], [2,3,3]
-        , [3,1,1], [3,1,2], [3,1,3]
-        , [3,2,1], [3,2,2], [3,2,3]
-        , [3,3,1], [3,3,2], [3,3,3]
-        ]
-
-
+    force $
+        map (\g -> (head g, length g)) . group . sort . map sum $
+            [ [1,1,1], [1,1,2], [1,1,3]
+            , [1,2,1], [1,2,2], [1,2,3]
+            , [1,3,1], [1,3,2], [1,3,3]
+            , [2,1,1], [2,1,2], [2,1,3]
+            , [2,2,1], [2,2,2], [2,2,3]
+            , [2,3,1], [2,3,2], [2,3,3]
+            , [3,1,1], [3,1,2], [3,1,3]
+            , [3,2,1], [3,2,2], [3,2,3]
+            , [3,3,1], [3,3,2], [3,3,3]
+            ]
+-- multiRollsSums :: [Int]
+-- multiRollsSums =
+--     force $ map sum
+--         [ [1,1]
+--         , [1,2]
+--         , [2,1]
+--         , [2,2]
+--         ]
+-- multiRollsSums :: [Int]
+-- multiRollsSums =
+--     [1..3]
 
 answer2 :: (Int, Int) -> Int
 answer2 winningGames =
@@ -135,4 +150,4 @@ answer2 winningGames =
 main :: IO ()
 main = do
     -- print . answer1 . play1 $ example
-    print . answer2 . play2 $ example
+    print . play2 $ example
