fd550c336ff39bfe55ec1cb145ad4e1d0af1fdcf
sylbru <sylvain.brunerie@gmail.com>
2021-12-29 19:16:41 +0100

    Day 25 done!

--- 25/advent.hs
+++ 25/advent.hs
@@ -1,13 +1,14 @@
-import Data.Map (Map)
-import qualified Data.Map.Strict as Map
+import qualified Data.List as List
 import qualified Data.Maybe as Maybe
 
-data SeaCucumber = EastFacing | SouthFacing
+data SeaCucumber = EastFacing | SouthFacing deriving (Eq, Show)
 type SeaFloor = [[Maybe SeaCucumber]]
 
 parseInput :: String -> SeaFloor
 parseInput input =
     map (map parseCell) . lines $ input
+
+-- instance Show SeaCucumber
 
 parseCell :: Char -> Maybe SeaCucumber
 parseCell char =
@@ -29,23 +30,30 @@ cellToString cell =
 
 move :: SeaFloor -> SeaFloor
 move floor =
-    moveSouthFacingHerd . moveEastFacingHerd $ floor
+    moveSouthFacingHerd . moveHerd EastFacing $ floor
 
 end :: [a] -> a
 end = head . reverse
 
-moveEastFacingHerd :: SeaFloor -> SeaFloor
-moveEastFacingHerd floor =
+moveHerd :: SeaCucumber -> SeaFloor -> SeaFloor
+moveHerd cucumberType floor =
     -- map ((drop 1) . (\row@(f:_) -> moveRow [] f (end row : row))) floor
     map (\row@(f:_) -> moveRow [] f row) floor
     where
         moveRow :: [Maybe SeaCucumber] -> Maybe SeaCucumber -> [Maybe SeaCucumber] -> [Maybe SeaCucumber]
-        moveRow acc first (Just EastFacing:Nothing:rest) =
-            moveRow (Just EastFacing:Nothing:acc) first rest
+        moveRow acc first (Just c:Nothing:rest) | c == cucumberType =
+            moveRow (Just c:Nothing:acc) first rest
         moveRow acc first (current:next:rest) =
             moveRow (current:acc) first (next:rest)
-        moveRow acc first (last:[]) =
-            reverse (last:acc)
+        moveRow acc Nothing (Just c:[]) | c == cucumberType =
+            setHead (Just c) . reverse $ (Nothing:acc)
+        moveRow acc _ (lst:[]) =
+            reverse (lst:acc)
+        moveRow acc _ [] =
+            reverse acc
+
+            -- >.
+            -- >.>.v.>>.
         --     case
         -- moveRow acc first (current:after:rest) =
         --     case (before, current) of
@@ -62,10 +70,37 @@ moveEastFacingHerd floor =
         -- moveRow acc first floor_@(_:after:rest) = floor_
 
 
+setHead :: a -> [a] -> [a]
+setHead newHead (_:tail) = (newHead:tail)
+
 moveSouthFacingHerd :: SeaFloor -> SeaFloor
-moveSouthFacingHerd = id
+moveSouthFacingHerd = List.transpose . moveHerd SouthFacing . List.transpose
+
+moveNSteps :: Int -> SeaFloor -> SeaFloor
+moveNSteps n floor =
+    (iterate move floor) !! n
+
+moveUntilSettled :: SeaFloor -> (SeaFloor, Int)
+moveUntilSettled floor =
+    go 0 floor
+    where
+        go :: Int -> SeaFloor -> (SeaFloor, Int)
+        go i f =
+            let step = move f
+            in
+            if step == f then
+                (step, i + 1)
+            else
+                go (i + 1) step
 
 main :: IO ()
 main = do
     raw <- getContents
-    putStrLn . seaFloorToString . move . parseInput $ "...>>>..."
+    -- putStrLn . seaFloorToString . move . parseInput $ ".>"
+    let (endFloor, steps) = moveUntilSettled . parseInput $ raw
+    print steps
+    putStrLn $ seaFloorToString endFloor
+    -- putStrLn . seaFloorToString . List.transpose . parseInput $ "...>>>...>\nv........."
+    -- putStrLn . seaFloorToString . moveHerd SouthFacing . List.transpose . parseInput $ "...>>>...>\nv........."
+    -- putStrLn . seaFloorToString . List.transpose . moveHerd SouthFacing . List.transpose . parseInput $ "...>>>...>\nv........."
+    -- putStrLn . seaFloorToString . move . parseInput $ "...>>>...>\nv........."
